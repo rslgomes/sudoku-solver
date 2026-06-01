0b20fec67d6fddb088418b776e50d8fda7a3b2dc
Rodrigo Gomes <rslgomes@gmail.com>
2026-06-01 01:33:12 +0100

    fix: linter extra space complains

--- src/shared/layouts/MainLayout/index.tsx
+++ src/shared/layouts/MainLayout/index.tsx
@@ -12,8 +12,8 @@ export default function MainLayout({ children, aside }: Props) {
     <div
       className="
         grid min-h-screen gap-y-4 gap-x-4 px-2 lg:px-4 lg:pb-4
-        [grid-template-areas:_'header_header'_'main_main']
-        lg:[grid-template-areas:_'header_header'_'aside_main']
+        [grid-template-areas:'header_header'_'main_main']
+        lg:[grid-template-areas:'header_header'_'aside_main']
         grid-cols-[20rem_1fr]
         grid-rows-[auto_1fr]
         bg-bg-page
@@ -28,13 +28,13 @@ export default function MainLayout({ children, aside }: Props) {
         ...
         <VisuallyHidden>Skip to main content</VisuallyHidden>
       </a>
-      <MainHeader className="[grid-area:_header]" />
+      <MainHeader className="[grid-area:header]" />
       {aside && (
-        <FABDialogOrAside className="[grid-area:_aside]">
+        <FABDialogOrAside className="[grid-area:aside]">
           <div className="px-4 py-2">{aside}</div>
         </FABDialogOrAside>
       )}
-      <main id="main-content" className="[grid-area:_main]" tabIndex={-1}>
+      <main id="main-content" className="[grid-area:main]" tabIndex={-1}>
         {children}
       </main>
     </div>
